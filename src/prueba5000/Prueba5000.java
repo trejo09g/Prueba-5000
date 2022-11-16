6cfc84cd252eafec6d92b6103a98ee988b001e4d
invitado <invitado@LAB010208.itchii.lab>
2022-11-16 13:40:29 -0700

    Carga inicial

--- src/prueba5000/Prueba5000.java
+++ src/prueba5000/Prueba5000.java
@@ -16,7 +16,18 @@ public class Prueba5000 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       String[]nombre = new String[5];
+        
+        nombre[0] = "Luis";
+        nombre[1] = "Maria";
+        nombre[2] = "Carlos";
+        nombre[3] = "Jose";
+        nombre[4] = "Ismael";
+        
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(nombre[i]);
+        }
+       
     }
     
 }
